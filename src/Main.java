cea03a08a99c88ff1b4904cc4af707dda6b83575
admin <titorenkodenis@mail.ru>
2022-10-30 20:59:37 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 5;
+        if (age >= 18) {
+            System.out.println("Ты можешь водить");
+        }
+        if (age < 18) {
+            System.out.println("Ты не можешь водить");
+        }
+
+    }
+}
